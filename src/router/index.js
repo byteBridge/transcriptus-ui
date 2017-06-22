6c2151e20f794eece406b5138fd7905112572c1b
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-22 22:39:19 +0200

    add the register and the secret to the routes array

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
+import Secret from '@/components/Secret'
 
 Vue.use(Router)
 
@@ -10,6 +12,18 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+
+    {
+      path: '/secret',
+      name: 'secret',
+      component: Secret
     }
   ]
 })
