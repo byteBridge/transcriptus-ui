ec7159c7c33374c3c3e6396aa98b291f22a31a18
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-22 15:57:46 +0200

    Include the menu component in the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
+    <my-menu></my-menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Menu from './components/Menu.vue'
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {MyMenu: Menu}
   }
 
 </script>
 
 <style type="text/css" src="@/assets/css/semantic-ui/semantic-ui.min.css"></style>
 <style type="text/css">
-  a {
+  a router-link {
     color: teal; /* Match with the site theme*/
   }
 </style>
