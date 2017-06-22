153e24b069fc06276b4a0747638f049c56d515a7
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-22 15:57:11 +0200

    Add menu (nav) with user detail

---
+++ src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ui teal segment">
+    <div class="ui secondary menu">
+        <a class="ui right item">
+        <div class="ui teal button ">
+          <label v-if="!authenticatedUser">Sign in</label>
+          {{authenticatedUser}}
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      authenticatedUser () {
+        return this.$store.getters.authenticatedUser
+      }
+    }
+
+  }
+</script>
