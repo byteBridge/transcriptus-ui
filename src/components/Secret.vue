f16e12554935e27e801982d7734d5ccdeb612d60
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-22 22:38:42 +0200

    add a protected route / secret

---
+++ src/components/Secret.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ui contaner">
+    <h2>The top10 most wanted people</h2>
+    <div class="ui vertical list">
+      <div class="item" v-for="criminal in criminals">
+        <div class="circular horizontal red label"><i class="user icon"></i></div>
+        {{criminal.username}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from '@/modules/axios'
+
+  export default {
+    'name': 'Secret',
+    data () {
+      return {
+        criminals: []
+      }
+    },
+
+    mounted () {
+      const auth = {
+        params: {
+          token: localStorage.getItem('token')
+        }
+      }
+      axios.get('/secret', auth)
+        .then(response => {
+          console.log(response)
+          this.criminals = response.data.users
+        })
+        .catch(console.log)
+    }
+  }
+</script>
